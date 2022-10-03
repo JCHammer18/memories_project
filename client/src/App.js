586dfa5ad5fa7f8f08929c6564786ca13f118735
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-10-03 02:23:53 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,12 @@ const App = () => {
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-
+                        <Grid item xs={12} sm={7}>
+                           <Posts /> 
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
                     </Grid>
                 </Container>
             </Grow>
