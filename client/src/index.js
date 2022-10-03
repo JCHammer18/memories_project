8ca9e12ba40f5762459c1b3bc81c82a01facf04f
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-03 12:23:28 -0400

    changed to configureStore in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import reducers from './reducers'
 
 import App from './App';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = configureStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
