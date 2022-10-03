6b329108719638d018ce066977ff2fa02aabe6a9
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-03 06:24:10 -0400

    added new info to className

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,7 +17,7 @@ const Form = () => {
     
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a Memory</Typography>
             <TextField name="creator" variant="outlined" label="Creator" fullwidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}  />
              <TextField name="title" variant="outlined" label="Title" fullwidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}  />
