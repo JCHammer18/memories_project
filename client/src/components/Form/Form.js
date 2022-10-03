1f2fa762fb64810841a16725dcea0e6cfe7c4569
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-03 05:56:12 -0400

    fixed value space

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -19,10 +19,10 @@ const Form = () => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a Memory</Typography>
-            <TextField name="creator" variant="outlined" label="Creator" fullwidthvalue={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}  />
-             <TextField name="title" variant="outlined" label="Title" fullwidthvalue={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}  />
-             <TextField name="message" variant="outlined" label="Message" fullwidthvalue={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}  />
-             <TextField name="tags" variant="outlined" label="tags" fullwidthvalue={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}  />
+            <TextField name="creator" variant="outlined" label="Creator" fullwidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}  />
+             <TextField name="title" variant="outlined" label="Title" fullwidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}  />
+             <TextField name="message" variant="outlined" label="Message" fullwidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}  />
+             <TextField name="tags" variant="outlined" label="tags" fullwidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}  />
             <div className={classes.fileInput}>
                 <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}>
                     <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit">Submit</Button>
