177c3c333cc601719fa01c5b1852c60537f1e960
Charlotte Strickland <charlottemarie81@gmail.com>
2022-10-03 16:40:30 -0400

    Revert "added new information to Posts.js"
    This reverts commit 791f7096aeaa84686fc2cb95f1c3b18debff9e1f.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 
@@ -10,18 +9,14 @@ const Posts = () => {
     const classes = useStyles();
 
     console.log(posts);
-
+    
     return (
-        !posts.length ? <CircularProgress /> : (
-            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
-                    <Grid key={post.id} item xs={12} sm={6}>
-                        <Post post={post} />
-                    </Grid>
-                ))}
-            </Grid>
-        )
+        <>  <h1 className={classes.something}>POSTS</h1>
+        <Post />
+        <Post />
+        </>
+       
     );
-}
+    }
 
 export default Posts;
